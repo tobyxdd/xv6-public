d6780e3f3f0ceb94a3c8703d75b4a90215e24a04
Toby <tobyxdd@gmail.com>
2019-11-08 17:35:03 -0800

    test progs

---
+++ prog1.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define TICKETS 30
+
+int main(int argc, char *argv[])
+{
+    tickets(TICKETS);
+    int i, k;
+    const int loop = 43000;
+    for (i = 0; i < loop; i++)
+    {
+        asm(nop); //in order to prevent the compiler from optimizing the for loop
+        for (k = 0; k < loop; k++)
+        {
+            asm(nop);
+        }
+    }
+    printf(1, "prog1 ticks: %d\n", ticks());
+    exit();
+}
